07ac0e748c1444198b0b37b754e9be1d7badad51
Eric Bidelman <ebidel@gmail.com>
2018-02-11 13:37:18 -0800

    change var name

--- public/firebaseHelper.mjs
+++ public/firebaseHelper.mjs
@@ -32,11 +32,10 @@ function setApp(firebaseApp) {
  */
 async function newPost(post) {
   const url = post.url;
-  const date = new Date(post.submitted);
+  const submitted = new Date(post.submitted);
   const year = String(date.getFullYear());
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-  const submitted = new Date(post.submitted);
 
   const doc = db.collection(year).doc(month);
   if (!(await doc.get()).exists) {
