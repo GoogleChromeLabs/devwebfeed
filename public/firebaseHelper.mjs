b852edfb525f6d328e1fceb349c95cf0e658ee99
Eric Bidelman <ebidel@gmail.com>
2018-02-11 16:13:03 -0800

    Fix missing date

--- public/firebaseHelper.mjs
+++ public/firebaseHelper.mjs
@@ -33,9 +33,9 @@ function setApp(firebaseApp) {
 async function newPost(post) {
   const url = post.url;
   const submitted = new Date(post.submitted);
-  const year = String(date.getFullYear());
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+  const year = String(submitted.getFullYear());
+  const month = String(submitted.getMonth() + 1).padStart(2, '0');
+  const day = String(submitted.getDate()).padStart(2, '0');
 
   const doc = db.collection(year).doc(month);
   if (!(await doc.get()).exists) {
